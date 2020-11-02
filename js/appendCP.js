42d7af8fddf5ddb0309dc20587bf3f80e2344730
jakubChaber <chaberjakub@gmail.com>
2020-11-02 17:34:56 +0100

    toggle sticky position

--- js/appendCP.js
+++ js/appendCP.js
@@ -1,26 +1,63 @@
-import { table, exTr, tableOfActiveObjs, tableWidth, tableStartPoint } from "./vars.js";
-import { setPosition                                                 } from "./setPosition.js"
+import { table, exTr, tableOfActiveObjs, tableWidth, tableStartPoint          } from "./vars.js";
+import { color} from "./methods.js";
+import { setPosition                                                          } from "./setPosition.js"
 var appendCP = () => {
 
     var table                = document.getElementById('exTable');
     var div                  = document.createElement("div");
     var button               = document.createElement("button");
+    var stickyButton         = document.createElement("button");
     var header               = document.createElement("header");
     var content              = document.createElement("div");
+    var selectAllBtn         = document.createElement("button");
 
-        header.id            = "exTabCounter";
-        content.id            = "exTabContent";
-        div.style.position   = "fixed";
-        div.style.display    = "flex";
+        header.id                 = "exTabCounter";
+        content.id                = "exTabContent";
+        selectAllBtn.id           = "sAllBtn"
+        selectAllBtn.innerText    = "select all";
+        stickyButton.innerText    = "get sticky";
+        div.style.position        = "fixed";
+        div.style.display         = "flex";
         div.style.flexDirection   = "column";
-        div.style.left       = `${tableWidth+tableStartPoint}px`;
-        div.style.top        = 0;
-        div.style.padding    = '5px';
-        div.style.boxSizing  = 'border-box';
-        div.style.cursor     = 'default';
-        div.style.background = 'rgba(32,114,69,.6)';
-        div.style.color      = '#eee';
-        div.style.height      = 'auto';
+        div.style.left            = `${tableWidth+tableStartPoint}px`;
+        div.style.top             = 0;
+        div.style.padding         = '5px';
+        div.style.boxSizing       = 'border-box';
+        div.style.cursor          = 'default';
+        div.style.background      = 'rgba(32,114,69,.6)';
+        div.style.color           = '#eee';
+        div.style.height          = 'auto';
+
+    var selectAllTr = () =>{
+     exTr.forEach(el => {
+
+        let arg = {
+            rowIndex: el.rowIndex,
+            obj     : el,
+            height  : 49,
+        }
+        color(arg, el.rowIndex);
+
+     });
+    }
+    var getSticky=()=>{
+        exTr.forEach(el => {
+            el.classList.toggle("exTr_sticky");
+         });
+    }
+    var cpInit=()=>{
+        div.id            = "controlPanel";
+        content.innerHTML = `0/${exTr.length}`;
+        header.innerHTML  = `0%`;
+        button.innerText  = "CSV";
+    
+        div.append(content);
+        div.append(header);
+        div.append(button);
+        div.append(selectAllBtn);
+        div.append(stickyButton);
+        table.append(div);
+    }
     var download =  (content, fileName, mimeType) => {
         var a    = document.createElement('a');
         mimeType = mimeType || 'application/octet-stream';
@@ -74,17 +111,12 @@ var appendCP = () => {
         download(csvContent, filename, 'text/csv;charset:utf-8');
     }
 
-    button.addEventListener('click', makeCP);
+    cpInit();
 
-    div.id            = "controlPanel";
-    content.innerHTML = `0/${exTr.length}`;
-    header.innerHTML  = `0%`;
-    button.innerText  = "CSV";
-    
-    div.append(content);
-    div.append(header);
-    div.append(button);
-    table.append(div);
+
+    button.addEventListener('click', makeCP);
+    selectAllBtn.addEventListener('click', selectAllTr);
+    stickyButton.addEventListener('click', getSticky);
 
 }
 export{
